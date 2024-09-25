f476c5d97b2527d39974257f242086ca9e950329
Milton <milton_pinheiro@outlook.com>
2024-09-25 14:05:00 -0400

    Added the players

--- entities.py
+++ entities.py
@@ -1,4 +1,7 @@
 from managers import Game_Manager
+from color import WHITE
+
+import pygame
 
 class Base():
     def __init__(self, layer):
@@ -22,8 +25,81 @@ class Base():
         pass
 
 class Player(Base):
-    def __init__(self):
+    '''
+    Class for the controlable players
+    
+    Parameters
+    ----------
+    controls_dict : dict
+        A dictionary with strings with the command
+        name as keys and keyboard keys as values
+
+    Notes
+    -----
+    Available command names are:
+        UP_MOVEMENT - Player upward movement button
+        DOWN_MOVEMENT - Player downward movement button
+        LEFT_MOVEMENT - Player left movement button
+        RIGHT_MOVEMENT - Player right movement button
+        UP_GUN - Gun upward movement button
+        DOWN_GUN - Gun downward movement button
+        SHOOT - Shooting button
+    '''
+    def __init__(self, x, y, controls_dict: dict):
         super().__init__(layer = 1)
+        self.controls = controls_dict
+
+        self.size = 0.05 * Game_Manager.screen_height
+
+        self.rect = pygame.rect.Rect(
+            x - self.size/2,
+            y - self.size/2,
+            self.size,
+            self.size,
+        )
+
+        self.moving_up = False
+        self.moving_down = False
+        self.aiming_up = False
+        self.aiming_down = False
+        self.shoot = False
+    
+    def event(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == self.controls["UP_MOVEMENT"]:
+                self.moving_up = True
+            if event.key == self.controls["DOWN_MOVEMENT"]:
+                self.moving_down = True
+            if event.key == self.controls["UP_GUN"]:
+                self.aiming_up = True
+            if event.key == self.controls["DOWN_GUN"]:
+                self.aiming_down = True
+            if event.key == self.controls["SHOOT"]:
+                self.shoot = True
+        if event.type == pygame.KEYUP:
+            if event.key == self.controls["UP_MOVEMENT"]:
+                self.moving_up = False
+            if event.key == self.controls["DOWN_MOVEMENT"]:
+                self.moving_down = False
+            if event.key == self.controls["UP_GUN"]:
+                self.aiming_up = False
+            if event.key == self.controls["DOWN_GUN"]:
+                self.aiming_down = False
+            if event.key == self.controls["SHOOT"]:
+                self.shoot = False
+
+    def process(self):
+        if self.moving_up and not self.moving_down:
+            self.rect.y -= 10
+            if self.rect.y <= 0:
+                self.rect.y = 0
+        if self.moving_down and not self.moving_up:
+            self.rect.y += 10
+            if self.rect.bottom >= Game_Manager.screen_height:
+                self.rect.bottom = Game_Manager.screen_height
+
+    def draw(self, screen):
+        pygame.draw.rect(surface = screen,color = WHITE,rect = self.rect)
 
 
 class Bomb(Base):
